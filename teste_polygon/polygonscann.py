c56029d4370651a60f577c4aadba3ce797251f37
samuelsm <samuelmeurer13@gmail.com>
2022-10-21 20:18:00 -0300

    algoritmo de criação de lista de holders, criada!

--- teste_polygon/polygonscann.py
+++ teste_polygon/polygonscann.py
@@ -11,21 +11,17 @@ vetor_to = list
 with PolygonScan("WDDZGYYWAGAC446V2JHQT93NW4F1TX6P9Q",False) as matic:
     save = (matic.get_erc721_token_transfer_events_by_contract_address_paginated("0x9928A8ea82d86290DfD1920E126B3872890525b3",1,10000,"asc"))
     i =0
-    '''
+    vetor_to = []
     for  i in range(len(save)):
         a = dict(save[i])
-        print(a)
-        print(type(a))
-        print(a.get("to"))
         b = a.get("to")
-        print(b)
-        print(type(b))
-#        vetor_to[i] = str(a.pop("to"))
-print(vetor_to)'''
+        vetor_to.append(b)
+        print(vetor_to)
 
-    lista = 0
+
+'''
     a = dict(save[2])
     b = a.get("to")
     samuel =[]
     samuel.append(b)
-    print(samuel)
+    print(samuel)'''
